bdb211e64ea57916d6368b3ec5f0295fa4cf4998
Restyled.io <commits@restyled.io>
2023-02-28 15:54:00 +0000

    Restyled by whitespace

--- images.g.ts
+++ images.g.ts
@@ -5,22 +5,22 @@ namespace myImages {
         switch(helpers.stringTrim(name)) {
             case "image1":
             case "AttackRight":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f f f f . . . . 
-. . . . f f e e e e f 2 f . . . 
-. . . f f e e e e f 2 2 2 f . . 
-. . . f e e e f f e e e e f . . 
-. . . f f f f e e 2 2 2 2 e f . 
-. . . f e 2 2 2 f f f f e 2 f . 
-. . f f f f f f f e e e f f f . 
-. . f f e 4 4 e b f 4 4 e e f . 
-. . f e e 4 d 4 1 f d d e f . . 
-. . . f e e e 4 d d d d f . . . 
-. . . . 4 d d e 4 4 4 e f . . . 
-. . . . e d d e 2 2 2 2 f . . . 
-. . . . f e e f 4 4 5 5 f f . . 
-. . . . f f f f f f f f f f . . 
-. . . . . f f . . . f f f . . . 
+. . . . . . . . . . . . . . . .
+. . . . . . f f f f f f . . . .
+. . . . f f e e e e f 2 f . . .
+. . . f f e e e e f 2 2 2 f . .
+. . . f e e e f f e e e e f . .
+. . . f f f f e e 2 2 2 2 e f .
+. . . f e 2 2 2 f f f f e 2 f .
+. . f f f f f f f e e e f f f .
+. . f f e 4 4 e b f 4 4 e e f .
+. . f e e 4 d 4 1 f d d e f . .
+. . . f e e e 4 d d d d f . . .
+. . . . 4 d d e 4 4 4 e f . . .
+. . . . e d d e 2 2 2 2 f . . .
+. . . . f e e f 4 4 5 5 f f . .
+. . . . f f f f f f f f f f . .
+. . . . . f f . . . f f f . . .
 `;
             case "p:95f_/WBhqX+L@P-pWe":
             case "Map":return img`
