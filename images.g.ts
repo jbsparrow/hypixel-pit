49d5954b0bb1530d7bc522a05a2c74a78687c07a
Jacob <admin@script-ware.net>
2023-03-01 09:10:31 -0500

    Update images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,196 @@ namespace myImages {
 . . . . f f f f f f f f f f . . 
 . . . . . f f . . . f f f . . . 
 `;
+                            case "image2":
+            case "Attack Facing away":return img`
+. . . . d 1 f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 2 2 f f f f f . . 
+. . f f e 2 e 2 2 e 2 e f f . . 
+. . f e 2 f 2 f f 2 f 2 e f . . 
+. . f f f 2 2 e e 2 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . . . f f f f f f f f 4 e . . 
+. . . . f 2 2 2 2 2 2 f d 4 . . 
+. . . . f 4 4 4 4 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image3":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Sword Attack Down":return img`
+. . . . . . . c c c c c . . . . 
+. . . . . . c c b b b c c . . . 
+. . . . . . . c c c c c . . . . 
+. . . . . . . . c d c . . . . . 
+. . . . . . . . c 1 c . . . . . 
+. . . . . . . c 1 1 9 c . . . . 
+. . . . . . . c 1 1 d c . . . . 
+. . . . . . . c 1 1 1 c . . . . 
+. . . . . . c 1 1 1 1 9 c . . . 
+. . . . . . c 1 1 c 1 1 c . . . 
+. . . . . . c 1 c 1 c 1 c . . . 
+. . . . . . c c 1 1 1 c c . . . 
+. . . . . . . c 1 1 d c . . . . 
+. . . . . . . c 1 1 9 c . . . . 
+. . . . . . . . c 9 c . . . . . 
+. . . . . . . . . c . . . . . . 
+`;
+            case "image5":
+            case "attack down no sword":return img`
+. . . . . . . f f . . . . . . . 
+. . . . . f f 2 2 f f . . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f 2 2 2 2 2 2 f f f . . 
+. . f f f 2 2 2 2 2 2 f f f . . 
+. . f e e e e e e e e e e f f . 
+. . f e 2 2 2 2 2 2 2 2 e f f . 
+. . f f f f e e e e f f f f f . 
+. . f e f b f 4 4 f b f e f f . 
+. . f e 4 1 f d d f 1 4 e f . . 
+. . f f e 4 d d d d 4 e f e . . 
+. . f e f 2 2 2 2 2 f 4 e . . . 
+. . f 4 f 4 4 5 5 4 f 4 e . . . 
+. . . . f f f f f f d d e . . . 
+. . . . . f f f f e d d e . . . 
+. . . . . . . . . . e e . . . . 
+`;
+            case "image7":
+            case "Attack Right":return img`
+. . . . . . . . f f . . . . . . 
+. . . . . f f f f 2 f f . . . . 
+. . . f f e e e e f 2 f f . . . 
+. . f f e e e e e f 2 2 f f . . 
+. . f e e e e f f e e e e f . . 
+. . f f f f f e e 2 2 2 2 e f . 
+. f f f e 2 2 2 f f f f e 2 f . 
+. f f f f f f f f e e e f f f . 
+. f e f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 b f d d e f c . 
+. . . f e e e 4 d d d e e . c c 
+. . . . f 2 2 2 2 e e d d e c 1 
+. . . . f 4 4 4 e 4 4 d d e c c 
+. . . . f f f f f e e e e . c . 
+. . . f f f f f f f f . . . . . 
+. . . f f f . . f f . . . . . . 
+`;
+            case "image9":
+            case "myImage0":return img`
+. . . . . . f f . . . . . . . . 
+. . . . f f 2 f f f f . . . . . 
+. . . f f 2 f e e e e f f . . . 
+. . f f 2 2 f e e e e e f f . . 
+. . f e e e e f f e e e e f . . 
+. f e 2 2 2 2 e e f f f f f . . 
+. f 2 e f f f f 2 2 2 e f f f . 
+. f f f e e e f f f f f f f f . 
+. f e e 4 4 f b e 4 4 e f e f . 
+. c f e d d f b 4 d 4 e e f . . 
+c c . e e d d d 4 e e e f . . . 
+1 c e d d e e 2 2 2 2 f . . . . 
+c c e d d 4 4 e 4 4 4 f . . . . 
+. c . e e e e f f f f f . . . . 
+. . . . . f f f f f f f f . . . 
+. . . . . . f f . . f f f . . . 
+`;
+            case "image8":
+            case "Sword Attack Left":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . c c c c c . . . . . . . 
+. . . c 9 c 1 1 1 c c c c . . . 
+. . c 1 1 1 c 1 1 1 1 1 1 c c c 
+. c 9 1 1 1 1 c 1 1 1 1 1 1 1 9 
+. . c d 1 1 c 1 1 1 9 d d c c c 
+. . . c 9 c d 1 1 c c c c . . . 
+. . . . c c c c c . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image11":
+            case "Sword Attack Up":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . c . . . . . . . . 
+. . . . . . c 1 c . . . . . . . 
+. . . . . c 1 1 9 c . . . . . . 
+. . . . c 1 1 1 1 9 c . . . . . 
+. . . . c c 1 1 1 c c . . . . . 
+. . . . c 1 c 1 c 9 c . . . . . 
+. . . . c 1 1 c d d c . . . . . 
+. . . . . c 1 1 9 c . . . . . . 
+. . . . . c 1 1 d c . . . . . . 
+. . . . . c 1 1 1 c . . . . . . 
+. . . . . c 1 1 d c . . . . . . 
+. . . . . . c d c . . . . . . . 
+. . . . . c c 9 c c . . . . . . 
+. . . . c c c c c c c . . . . . 
+`;
+            case "image6":
+            case "Sword Attack Right":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . c c c c c . . . . 
+. . . c c c c 1 1 1 c 9 c . . . 
+c c c 1 1 1 1 1 1 c 1 1 1 c . . 
+9 1 1 1 1 1 1 1 c 1 1 1 1 9 c . 
+c c c d d 9 1 1 1 c 1 1 d c . . 
+. . . c c c c 1 1 d c 9 c . . . 
+. . . . . . . c c c c c . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "Unknown sword asset":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . c . . . . . . 
+. . . . . . . . . c 1 1 . . . . 
+. . . . . . . . . c 1 1 . . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c 1 1 c . . . 
+. . . . . . . . . c c c c . . . 
+. . . . . . . . c c b b c c . . 
+. . . . . . . . . c c c c . . . 
+`;
             case "p:95f_/WBhqX+L@P-pWe":
             case "Map":return img`
 666666666666666666666667777777777777777777777777777777777777777777777777777777777777777777777788888888888888888888888888888888833333333333333333333333333333333333333
